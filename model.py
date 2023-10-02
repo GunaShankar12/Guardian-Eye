81d791a346f22a73f72144c7bd44b322753c8c38
SKRiyaz03 <shaikriyaz.20.csm@anits.edu.in>
2023-10-02 20:20:17 +0530

    threshold need to be modified

--- model.py
+++ model.py
@@ -1,49 +1,68 @@
-from retinaface import RetinaFace
-import time
 import cv2
-import matplotlib.pyplot as plt
-img_path = './images/ds_test1.jpg'
+from insightface.app import FaceAnalysis
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
-start_time = time.time()
-img = cv2.imread(img_path)
+recognition = FaceAnalysis(name='buffalo_l')
+recognition.prepare(ctx_id=0, det_thresh=0.6, det_size=(640, 640))
+data = pd.read_csv('data.csv')
+required = data.iloc[:, 1:].values
+THRESHOLD = 0.4
+
+
+def detect_faces(frame):
+    faces= recognition.get(frame)
+    face_landmarks = []
+    for face in faces:
+        face_landmarks.append((face.embedding, face.bbox)) # face is a dictonary with keys:['bbox', 'kps', 'det_score', 'landmark_3d_68', 'pose', 'landmark_2d_106', 'gender', 'age', 'embedding']
+    return face_landmarks
+
+
+def compare_embeddings(new_embedding):
+    for embedding in required:
+        label = embedding[0]
+        embedding = embedding[1:]
+        s = cosine_similarity([embedding], [new_embedding])
+        if(s >= THRESHOLD):
+            print(s, label)
+            return label
+    return None
+
+
+
+def webcam():
+    video_capture = cv2.VideoCapture(0)
+    try:
+        while True:
+            ret, frame = video_capture.read()
+            face_landmarks = detect_faces(frame)
+            for face_landmark, bbox in face_landmarks:
+                tag = compare_embeddings(face_landmark)
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 2)
+                if(tag is not None):
+                    cv2.putText(frame, tag, (int(bbox[0]), int(bbox[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('Video Capture', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except KeyboardInterrupt:
+        pass
+
+
+def static_recognition(img_path):
+    frame = cv2.imread(img_path)
+    face_landmarks = detect_faces(frame)
+    for face_landmark, bbox in face_landmarks:
+        tag = compare_embeddings(face_landmark)
+        cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 2)
+        if(tag is not None):
+            cv2.putText(frame, tag, (int(bbox[0]), int(bbox[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('Video Capture', frame)
+    cv2.waitKey(0)
+
 '''
-output for one face
-{
-    'score': 0.9994959831237793,
-    'facial_area': [200, 238, 221, 264],
-    'landmarks': 
-        {
-            'right_eye': [207.93968, 247.9548],
-            'left_eye': [217.21861, 248.2999],
-            'nose': [213.07979, 252.90683], 
-            'mouth_right': [208.32773, 257.35257], 
-            'mouth_left': [216.09364, 257.66913]
-        }
-}
+to run without warnings: 
+python -W ignore <filename>.py
 '''
-obj = RetinaFace.detect_faces(img_path)
-end_time = time.time()
-print("Detection Time: ", end_time - start_time)
-
-
-for key in obj.keys():
-    identity = obj[key]
-    # print(identity)
-    facial_area = identity['facial_area']
-    cv2.rectangle(img, (facial_area[2], facial_area[3]), (facial_area[0], facial_area[1]), (0, 255, 0), 2)
-    break
-plt.imshow(img[:,:,::-1])
-plt.show()
-
-plt.imsave("./images/ds_test1_result.jpg", img[:,:,::-1])
-
-
-# face recognition
-from deepface import DeepFace
-img_base = "./images/ds_base.jpg"
-img_test = "./images/ds_test1.jpg"
-start_time = time.time()
-result = DeepFace.verify(img1_path = img_base, img2_path = img_test, model_name = 'Facenet', distance_metric = 'euclidean_l2')
-end_time = time.time()
-print("Recoginition Time: ", end_time - start_time)
-print("result", result)
+if __name__ == '__main__':
+    # static_recognition('./images/group.png')
+    webcam()
